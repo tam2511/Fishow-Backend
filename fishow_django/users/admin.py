8812f01cdfae6b37de085e4d73e9471cbc760a95
DamirAkzhigitov <damir.ckelet@gmail.com>
2020-03-08 12:49:33 +0300

    Initial commit

---
+++ fishow_django/users/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from users.models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    # add_form =
+    # form =
+    model = User
+    list_display = ['username', 'email', 'is_staff']
+
+
+admin.site.register(User, UserAdmin)
