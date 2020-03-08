a7aecc8d4a3c4dbbecef896943f1ee0704d1fe85
DamirAkzhigitov <damir.ckelet@gmail.com>
2020-03-08 13:05:52 +0300

    urls

--- fishow_django/fishow_django/urls.py
+++ fishow_django/fishow_django/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = (
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('api-auth/', include('rest_framework.urls')),
@@ -25,4 +25,4 @@ urlpatterns = (
     path('api/rest-auth/registration/', include('rest_auth.registrations.urls')),
     # url(r'^rest-auth/', include('rest_auth.urls')),
     # url(r'^rest-auth/registration/', include('rest_auth.registration.urls'))
-)
+]
