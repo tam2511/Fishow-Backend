313f2fa0f408e9785532cfc25ea2f219c0313040
DamirAkzhigitov <damir.ckelet@gmail.com>
2020-03-08 14:43:21 +0300

    urls forms

--- fishow_django/fishow_django/urls.py
+++ fishow_django/fishow_django/urls.py
@@ -17,8 +17,16 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 
+from django_registration.backends.one_step.views import RegistrationView
+
+from users.forms import CustomUserForm
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/register/', RegistrationView.as_view(
+        form_class=CustomUserForm,
+        success_url='/',
+        ), name='django_registration_register'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('api/rest-auth/', include('rest_framework.urls')),
