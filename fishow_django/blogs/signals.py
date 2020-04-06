6425deb959f0e41d3011ccdce9ec9ee07ed150b2
vd <irnbru007@mail.ru>
2020-04-06 22:08:54 +0300

    comment and blog count changing

--- fishow_django/blogs/signals.py
+++ fishow_django/blogs/signals.py
@@ -1,9 +1,12 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save,pre_delete
 from django.dispatch import receiver
 from django.utils.text import slugify
 
 from core.utils import generate_random_string
-from blogs.models import Blog
+from blogs.models import Blog,Comment
+
+from django.contrib.auth import get_user_model
+from users.models import CustomUser
 
 @receiver(pre_save, sender=Blog)
 def add_slug_to_blog(sender, instance,*args,**kwargs):
@@ -11,3 +14,27 @@ def add_slug_to_blog(sender, instance,*args,**kwargs):
         slug = slugify(instance.title)
         random_string = generate_random_string()
         instance.slug = slug + '-' + random_string
+
+@receiver(pre_save, sender=Blog)
+def count_plus_blog(sender, instance,*args,**kwargs):
+    user=CustomUser.objects.get(username=instance.author)
+    user.count_blogs=int(user.count_blogs)+1
+    user.save()
+
+@receiver(pre_delete, sender=Blog)
+def dell_count_blog(sender, instance,*args,**kwargs):
+    user=CustomUser.objects.get(username=instance.author)
+    user.count_blogs=int(user.count_blogs)-1
+    user.save()
+
+@receiver(pre_save, sender=Comment)
+def count_plus_blog(sender, instance,*args,**kwargs):
+    user=CustomUser.objects.get(username=instance.author)
+    user.count_comments=int(user.count_comments)+1
+    user.save()
+
+@receiver(pre_delete, sender=Comment)
+def dell_count_blog(sender, instance,*args,**kwargs):
+    user=CustomUser.objects.get(username=instance.author)
+    user.count_comments=int(user.count_comments)-1
+    user.save()
