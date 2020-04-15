9df796a5929ee33970980447e1631d60082021a4
AlexanderOfMacedon <48325302+AlexanderOfMacedon@users.noreply.github.com>
2020-04-15 20:11:44 +0300

    обновил примеры для славика

--- fishow_django/mysql_utils.py
+++ fishow_django/mysql_utils.py
@@ -14,10 +14,23 @@ def escape_name(s):
 con = pymysql.connect('localhost', 'tam2511_fishow',
                       '081099', 'tam2511_fishow')
 
-dict_of_params = {'time': 23, 'temperature': 7, 'wind': 2, 'wind_direction': 'СЗ', 'gust': 0, 'phenomenon': "['пасмурно', ' небольшой дождь']", 'pressure': 748, 'humidity': 0.26, 'uv_index': 1, 'moon_direction': 1, 'moon': 0.82, 'day': 95, 'date': datetime.strptime('04.04.2020','%d.%m.%Y'),
-                  'areal': 'московская область', 'city': 'город', 'probs': "{'щука': {'prob': 0.7, 'importance_features': {'names': [('wind', 0, 0), ('gust', 1, 3), ('gust', 0, 0), ('sub_temperature', 2, 0)], 'values': [3.0, 19.0, 9.0, -2.0]}},"
-                                                                           "'судак': {'prob': 0.6, 'importance_features': {'names': [('temperature', 0, 15), ('humidity', 1, 12), ('gust', 0, 0), ('gust', 2, 9)], 'values': [-4.0, 0.76, 9.0, 0.0]}}"}
-names = list(dict_of_params)
+dict_of_params = [{'time': 23, 'temperature': 7, 'wind': 2, 'wind_direction': 'СЗ', 'gust': 0,
+                  'phenomenon': "['пасмурно', ' небольшой дождь']", 'pressure': 748, 'humidity': 0.26, 'uv_index': 1,
+                  'moon_direction': 1, 'moon': 0.82, 'day': 95, 'date': datetime.strptime('04.04.2020','%d.%m.%Y'),
+                  'areal': 'московская область', 'city': 'москва', 'prob': 0.5, 'fish': 'щука', 'features': "признаки ляляля"},
+                  {'time': 12, 'temperature': 7, 'wind': 2, 'wind_direction': 'СЗ', 'gust': 0,
+                  'phenomenon': "['пасмурно', ' небольшой дождь']", 'pressure': 748, 'humidity': 0.26, 'uv_index': 1,
+                  'moon_direction': 1, 'moon': 0.82, 'day': 95, 'date': datetime.strptime('04.04.2020','%d.%m.%Y'),
+                  'areal': 'ленинградская область', 'city': 'питер', 'prob': 0.5, 'fish': 'карп', 'features': "признаки ляляля"},
+                  {'time': 0, 'temperature': 7, 'wind': 2, 'wind_direction': 'СЗ', 'gust': 0,
+                  'phenomenon': "['пасмурно', ' небольшой дождь']", 'pressure': 748, 'humidity': 0.26, 'uv_index': 1,
+                  'moon_direction': 1, 'moon': 0.82, 'day': 95, 'date': datetime.strptime('04.04.2020','%d.%m.%Y'),
+                  'areal': 'московская область', 'city': 'не москва', 'prob': 0.5, 'fish': 'сом', 'features': "признаки ляляля"},
+                  {'time': 3, 'temperature': 7, 'wind': 2, 'wind_direction': 'СЗ', 'gust': 0,
+                  'phenomenon': "['пасмурно', ' небольшой дождь']", 'pressure': 748, 'humidity': 0.26, 'uv_index': 1,
+                  'moon_direction': 1, 'moon': 0.82, 'day': 95, 'date': datetime.strptime('04.04.2020','%d.%m.%Y'),
+                  'areal': 'московская область', 'city': 'москва', 'prob': 0.5, 'fish': 'сом', 'features': "признаки ляляля"},
+                  
 cols = ', '.join(map(escape_name, names))  # assumes the keys are *valid column names*.
 placeholders = ', '.join(['%({})s'.format(name) for name in names])
 
