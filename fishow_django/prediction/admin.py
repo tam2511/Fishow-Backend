9290d13acf0bedc4b6ef4c39c018d89bb87a9a8d
vd <irnbru007@mail.ru>
2020-04-15 22:55:40 +0300

    predictionten

--- fishow_django/prediction/admin.py
+++ fishow_django/prediction/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from prediction.models import Prediction
+from prediction.models import Prediction,Predictionten
 
 
 admin.site.register(Prediction)
+admin.site.register(Predictionten)
 # Register your models here.
