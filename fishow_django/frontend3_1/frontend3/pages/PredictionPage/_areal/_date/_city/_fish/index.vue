2e5b1696feca581c5c9a515a83a9c1f4b47cc4c2
theoffice <damir.ckelet@gmail.com>
2020-05-23 19:19:43 +0300

    выбор области и города

--- fishow_django/frontend3_1/frontend3/pages/PredictionPage/_areal/_date/_city/_fish/index.vue
+++ fishow_django/frontend3_1/frontend3/pages/PredictionPage/_areal/_date/_city/_fish/index.vue
@@ -1,12 +1,14 @@
-<template>
-  <div class="fish">
-    <ul>
-      {{
-        predictions
-      }}
-      <!--      <li v-for="prediction in predictions" :key="prediction.id"></li>-->
-    </ul>
-  </div>
+<template lang="pug">
+  div.container
+    div.row.row-50
+      div.col-lg-8
+        div.fishow-prediction
+          ul
+            li(v-for="prediciton in predictions" :key="prediciton.id") {{prediciton}}
+      div.col-lg-4
+        div.fishow-sidebar
+          ul
+            li(v-for="i in 25" :key="i") {{ i }}
 </template>
 
 <script>
@@ -17,10 +19,11 @@ export default {
       const date = route.params.date
       const city = route.params.city
       const areal = route.params.areal
-      const response = await $axios.get(
+      const url = encodeURI(
         `/predictionten/?areal=${areal}&date=${date}&city=${city}&fish=${fish}`
       )
-      return { predictions: response.data }
+      const response = await $axios.get(url)
+      return { predictions: response.data.results[0] }
     } catch (e) {
       console.log('e =', e)
     }
@@ -36,4 +39,26 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.fish {
+  display: flex;
+  flex-flow: row;
+}
+.fishow-sidebar {
+  position: fixed;
+  margin: 70px 70px 0 70px;
+  padding: 10px;
+  min-width: 200px;
+  right: 0;
+  top: 0;
+  background: var(--background-color-primary);
+  border: 1px solid var(--background-color-border);
+}
+.fishow-prediction {
+  min-height: 2000px;
+  position: relative;
+  li {
+    min-height: 100px;
+  }
+}
+</style>
