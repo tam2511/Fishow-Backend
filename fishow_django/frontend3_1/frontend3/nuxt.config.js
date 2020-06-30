97fd4197ba9fdffadc932018d9db891cc46604e1
theoffice <damir.ckelet@gmail.com>
2020-06-30 19:25:49 +0300

    yandex turbo

--- fishow_django/frontend3_1/frontend3/nuxt.config.js
+++ fishow_django/frontend3_1/frontend3/nuxt.config.js
@@ -15,6 +15,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'yandex-verification', content: 'cb7a2c560ce5c37b' },
       {
         hid: 'description',
         name: 'description',
