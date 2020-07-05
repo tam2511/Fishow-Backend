f11cdee4e69b35df401918436427e857c3c9c810
vd <irnbru007@mail.ru>
2020-07-05 21:59:13 +0300

    1 вариант восстановления пароля

--- fishow_django/users/views.py
+++ fishow_django/users/views.py
@@ -4,11 +4,22 @@ from django.shortcuts import render
 from allauth.account.adapter import DefaultAccountAdapter
 from django.conf import settings
 
+from allauth.account.views import PasswordResetView
+
+from django.conf import settings
+from django.dispatch import receiver
+from django.http import HttpRequest
+from django.middleware.csrf import get_token
+
 class DefaultAccountAdapterCustom(DefaultAccountAdapter):
 
     def send_mail(self, template_prefix, email, context):
-        context['activate_url'] = settings.URL_FRONT + \
-            'account-confirm-email/' + context['key'] + '/'
+        try:
+            context['activate_url'] = settings.URL_FRONT + \
+                'account-confirm-email/' + context['key'] + '/'
+        except:
+            pass
+#         context['password_reset_url'] = settings.URL_FRONT + \
+#             'api/rest-auth/password/reset/confirm/'+ context['token'] + '/'
         msg = self.render_mail(template_prefix, email, context)
         msg.send()
-
