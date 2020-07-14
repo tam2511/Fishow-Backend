9a20693dc701deb5020cf808af5f25a0a27105a4
AlecanderOfMacedon <tam2511@mail.ru>
2020-07-14 22:04:36 +0300

    разделение на модули

---
+++ fishow_django/prediction/utils/temperature_generator.py
@@ -0,0 +1,49 @@
+import datetime
+
+from .helper import *
+
+
+class TemperatureTextGenerator:
+    template_text_one = '''Как правило, температура воздуха несильно влияет на клев {}. 
+        Это можно объяснить тем, что есть факторы, которые влияют на клев более существенно.'''
+
+    template_text_ten = '''Как правило, температура воздуха несильно влияет на клев {}. 
+            Это можно объяснить тем, что есть факторы, которые влияют на клев более существенно.'''
+
+    template_text_fork_one = '''Однако, по нашим прогнозам изменения температуры воздуха за сегодня могут
+                 повлиять на клев {}.'''
+
+    template_text_fork_ten = '''Однако, по нашим прогнозам изменения температуры воздуха за сегодня могут
+                     повлиять на клев {}.'''
+
+    @staticmethod
+    def day_temperature_text_generate(data, date, fish):
+        '''
+        Генерация текста для одного дня для блока температура на основе 1-3 дневного прогноза.
+        :param data: Список строк из базы данных для фиксированной местности
+        :param date: Текущая дата
+        :param fish: Текущий вид рыбы
+        :return: Текст
+        '''
+        influence_time = get_influence_time(data, date, fish)
+        text_builder = TemperatureTextGenerator.template_text_one.format(cases[fish]['r'])
+        if len(influence_time) > 0:
+            text_builder += TemperatureTextGenerator.template_text_fork_one.format(
+                influence_text_generate(influence_time))
+        return text_builder
+
+    @staticmethod
+    def ten_day_temperature_text_generate(data, date, fish):
+        '''
+        Генерация текста для девяти суток для блока температура на основе десяти дневного прогноза.
+        :param data: Список строк из базы данных для фиксированной местности
+        :param date: Текущая дата
+        :param fish: Текущий вид рыбы
+        :return: Текст
+        '''
+        influence_days = get_influence_days(data, date, fish)
+        text_builder = TemperatureTextGenerator.template_text_ten.format(cases[fish]['r'])
+        if len(influence_days) > 0:
+            text_builder += TemperatureTextGenerator.template_text_fork_ten.format(
+                influence_tendays_text_generate(influence_days))
+        return text_builder
--- fishow_django/prediction/utils/text_generator.py
+++ fishow_django/prediction/utils/text_generator.py
@@ -1,101 +1,4 @@
-import datetime
-
-
-def morning_influence(influence_time):
-    return (0, 6) in influence_time or (0, 9) in influence_time
-
-
-def day_influence(influence_time):
-    return (0, 12) in influence_time or (0, 15) in influence_time
-
-
-def evening_influence(influence_time):
-    return (0, 18) in influence_time or (0, 21) in influence_time
-
-
-def night_influence(influence_time):
-    return (0, 0) in influence_time or (0, 3) in influence_time
-
-
-def feature_influence(influence_time):
-    return sum([_[0] for _ in influence_time]) > 0
-
-def parse_date(date):
-    date = date.split('.')
-    day = date[0]
-    month = date[1]
-    month_map = {
-        '1': 'января', '2': 'февраля', '3': 'марта',
-        '4': 'апреля', '5': 'мая', '6': 'июня',
-        '7': 'июля', '8': 'августа', '9': 'сентября',
-        '10': 'октября', '11': 'ноября', '12': 'декабря'
-    }
-    return '{} {}'.format(day, month_map[month])
-
-
-def influence_text_generate(influence_time):
-    text_builder = ''
-    morning, day, evening, night = morning_influence(influence_time), day_influence(influence_time), evening_influence(
-        influence_time), night_influence(influence_time)
-    if morning or day or evening or night:
-        text_builder += 'сегодня '
-        words = []
-        if morning:
-            words.append('утром')
-        if day:
-            words.append('днем')
-        if evening:
-            words.append('вечером')
-        if night:
-            words.append('ночью')
-        if feature_influence(influence_time):
-            text_builder += ', '.join(words)
-            text_builder += 'и на клев в ближайшие трое суток'
-        elif len(words) == 1:
-            text_builder += (words[0]) + ''
-        else:
-            text_builder += ', '.join(words[:-1]) + 'и {}'.format(words[-1])
-    else:
-        text_builder += 'в ближайшие трое суток'
-
-def influence_tendays_text_generate(influence_time):
-    days = list(set(influence_time))
-    if len(days) == 1:
-        return parse_date(days[0])
-    return ', '.join([parse_date(_) for _ in days[:-1]]) + ' и {}'.format(parse_date(days[-1]))
-
-
-cases = {
-    'щука': {'r': 'щуки'},
-    'сом': {'r': 'сома'},
-    'судак': {'r': 'судака'},
-    'окунь': {'r': 'окуня'},
-    'берш': {'r': 'берша'},
-    'речная форель': {'r': 'речной форели'},
-    'озерная форель': {'r': 'озерной форели'},
-    'елец': {'r': 'ельца'},
-    'чехонь': {'r': 'чехони'},
-    'голавль': {'r': 'голавля'},
-    'язь': {'r': 'язя'},
-    'карп': {'r': 'карпа'},
-    'жерех': {'r': 'жереха'},
-    'лещ': {'r': 'леща'},
-    'карась': {'r': 'карася'},
-    'линь': {'r': 'линя'},
-    'пескарь': {'r': 'пескаря'},
-    'ротан': {'r': 'ротана'},
-    'плотва': {'r': 'плотвы'},
-    'красноперка': {'r': 'красноперки'},
-    'налим': {'r': 'налима'},
-    'густера': {'r': 'густеры'},
-    'амур': {'r': 'амура'},
-    'ерш': {'r': 'ерша'},
-    'сазан': {'r': 'сазана'},
-    'подуст': {'r': 'подуста'},
-    'толстолобик': {'r': 'толстолобика'},
-    'вобла': {'r': 'воблы'},
-}
-
+from .temperature_generator import TemperatureTextGenerator
 
 class TextGenerator:
     '''
@@ -110,58 +13,13 @@ class TextGenerator:
         TextGenerator.current_areal = areal
 
     @staticmethod
-    def day_temperature_text_generate(data, date, fish):
-        '''
-        Генерация текста для одного дня для блока температура на основе 1-3 дневного прогноза.
-        :param data: Список строк из базы данных для фиксированной местности
-        :param date: Текущая дата
-        :param fish: Текущий вид рыбы
-        :return: Текст
-        '''
-        observe_dates = [date - datetime.timedelta(days=day) for day in range(4)]
-        filtred_data = {observe_date: [(eval(_.feature), _.temperature, _.time) for _ in data if
-                                       _.date == observe_date and _.fish == fish] for observe_date in observe_dates}
-        influence = []
-        for index, observe_date in enumerate(observe_dates):
-            explain_prediction = sorted(filtred_data[observe_date], key=lambda x: x[-1])
-            for features, temperature, time in explain_prediction:
-                for feature, value, weight in features:
-                    feature_name, day_shift, time_shift = feature
-                    if 0 <= index * 24 + time - (day_shift * 24 + time_shift) < 24 and feature_name == 'temperature':
-                        influence.append((index, time, weight, value))
-        influence = sorted(influence)
-        influence_time = [(_[0], _[1]) for _ in influence]
-        text_builder = '''Как правило, температура воздуха несильно влияет на клев {}. 
-        Это можно объяснить тем, что есть факторы, которые влияют на клев более существенно.'''.format(cases[fish]['r'])
-        if len(influence_time) > 0:
-            text_builder += '''Однако, по нашим прогнозам изменения температуры воздуха за сегодня могут
-             повлиять на клев {}.'''.format(influence_text_generate(influence_time))
-        return text_builder
+    def check_stage(city, areal):
+        return TextGenerator.current_city == city and TextGenerator.current_areal == areal
 
     @staticmethod
-    def ten_day_temperature_text_generate(data, date, fish):
-        '''
-        Генерация текста для девяти суток для блока температура на основе десяти дневного прогноза.
-        :param data: Список строк из базы данных для фиксированной местности
-        :param date: Текущая дата
-        :param fish: Текущий вид рыбы
-        :return: Текст
-        '''
-        observe_dates = [date - datetime.timedelta(days=day) for day in range(9)]
-        filtred_data = {observe_date: [(eval(_.feature), _.temperature, _.time) for _ in data if
-                                       _.date == observe_date and _.fish == fish] for observe_date in observe_dates}
-        influence = []
-        for index, observe_date in enumerate(observe_dates):
-            explain_prediction = sorted(filtred_data[observe_date], key=lambda x: x[-1])
-            for features, temperature, time in explain_prediction:
-                for feature, value, weight in features:
-                    feature_name, day_shift, time_shift = feature
-                    if 0 <= index * 24 + time - (day_shift * 24 + time_shift) < 24 and feature_name == 'temperature':
-                        influence.append((observe_date, time, weight, value))
-        influence_days = [_[0] for _ in influence]
-        text_builder = '''Как правило, температура воздуха несильно влияет на клев {}. 
-        Это можно объяснить тем, что есть факторы, которые влияют на клев более существенно.'''.format(cases[fish]['r'])
-        if len(influence_days) > 0:
-            text_builder += '''Однако, по нашим прогнозам изменения температуры воздуха за эту неделю могут
-             повлиять на клев {}.'''.format(influence_tendays_text_generate(influence_days))
-        return text_builder
+    def temperature_one(data, date, fish):
+        return TemperatureTextGenerator.day_temperature_text_generate(data, date, fish)
+
+    @staticmethod
+    def temperature_ten(data, date, fish):
+        return TemperatureTextGenerator.ten_day_temperature_text_generate(data, date, fish)
