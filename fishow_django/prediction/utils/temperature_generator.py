64fb31156f3eac57fe0795c676b1e86e228b5557
AlecanderOfMacedon <tam2511@mail.ru>
2020-07-19 16:56:12 +0300

    поправил текст для блока температура, добавил в словарь cases влияние температуры на рыбу

--- fishow_django/prediction/utils/temperature_generator.py
+++ fishow_django/prediction/utils/temperature_generator.py
@@ -2,17 +2,17 @@ from .helper import *
 
 
 class TemperatureTextGenerator:
-    template_text_one = '''Как правило, температура воздуха несильно влияет на клев {}. 
-        Это можно объяснить тем, что есть факторы, которые влияют на клев более существенно.'''
+    template_text_one = '''Сама по себе температура воздуха несильно влияет на клев рыбы. При резком понижении или повышении температуры прогноз клева может изменится. Это можно объяснить тем, что температура воздуха влияет на клев рыбы косвенно через температуру воды. Поэтому при небольших колебаниях температуры воздуха температура воды не успевает изменится настолько, чтобы повлияет на клев в тот промежуток времени, по которому делается прогноз нашей моделью. '''
 
-    template_text_ten = '''Как правило, температура воздуха несильно влияет на клев {}. 
-            Это можно объяснить тем, что есть факторы, которые влияют на клев более существенно.'''
+    template_text_ten = '''Сама по себе температура воздуха несильно влияет на клев рыбы. При резком понижении или повышении температуры прогноз клева может изменится. Это можно объяснить тем, что температура воздуха влияет на клев рыбы косвенно через температуру воды. Поэтому при небольших колебаниях температуры воздуха температура воды не успевает изменится настолько, чтобы повлияет на клев в тот промежуток времени, по которому делается прогноз нашей моделью. '''
 
-    template_text_fork_one = '''Однако, по нашим прогнозам изменения температуры воздуха за сегодня могут
-                 повлиять на клев {}.'''
+    template_text_pos_influence = '''Наиболее благоприятные условия для ловли {}: *{}* температуры воздуха.'''
 
-    template_text_fork_ten = '''Однако, по нашим прогнозам изменения температуры воздуха за сегодня могут
-                     повлиять на клев {}.'''
+    template_text_neg_influence = '''Неблагоприятные условия для ловли {}: *{}* температуры воздуха.'''
+
+    template_text_fork_one = '''По нашим прогнозом, изменения температуры воздуха за {} может повлиять на клев {} {}.'''
+
+    template_text_fork_ten = '''По нашим прогнозом, изменения температуры воздуха наиболее сильно повлияют на клев {} {}.'''
 
     @staticmethod
     def day_text_generate(data, date, fish):
@@ -24,7 +24,13 @@ class TemperatureTextGenerator:
         :return: Текст
         '''
         influence_time = get_influence_time(data, date, fish, 'temperature')
-        text_builder = TemperatureTextGenerator.template_text_one.format(cases[fish]['r'])
+        text_builder = TemperatureTextGenerator.template_text_one
+        if cases[fish]['temperature+']:
+            text_builder += TemperatureTextGenerator.template_text_pos_influence.format(cases[fish]['r'],
+                                                                                        cases[fish]['temperature+'])
+        if cases[fish]['temperature-']:
+            text_builder += TemperatureTextGenerator.template_text_neg_influence.format(cases[fish]['r'],
+                                                                                        cases[fish]['temperature-'])
         if len(influence_time) > 0:
             text_builder += TemperatureTextGenerator.template_text_fork_one.format(
                 influence_text_generate(influence_time))
@@ -40,7 +46,13 @@ class TemperatureTextGenerator:
         :return: Текст
         '''
         influence_days = get_influence_days(data, date, fish, 'temperature')
-        text_builder = TemperatureTextGenerator.template_text_ten.format(cases[fish]['r'])
+        text_builder = TemperatureTextGenerator.template_text_ten
+        if cases[fish]['temperature+']:
+            text_builder += TemperatureTextGenerator.template_text_pos_influence.format(cases[fish]['r'],
+                                                                                        cases[fish]['temperature+'])
+        if cases[fish]['temperature-']:
+            text_builder += TemperatureTextGenerator.template_text_neg_influence.format(cases[fish]['r'],
+                                                                                        cases[fish]['temperature-'])
         if len(influence_days) > 0:
             text_builder += TemperatureTextGenerator.template_text_fork_ten.format(
                 influence_tendays_text_generate(influence_days))
