e40f2893c2e1830998f2e6428efd5df88390cf74
DamirAkzhigitov <damir.ckelet@gmail.com>
2020-07-20 23:08:20 +0300

    moon pic

--- fishow_django/frontend3_1/frontend3/components/predictPage/Results/Moon/OneDay/MoonOne.vue
+++ fishow_django/frontend3_1/frontend3/components/predictPage/Results/Moon/OneDay/MoonOne.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <day-block :norm-day="normDay" :day-calendar="dayCalendar" />
-    {{ day.moon }}
+    <!--    {{ day.moon }}-->
+    <!--    {{ day.moon_direction }}-->
+    <figure class="image is-48x48">
+      <img :src="moonPic" />
+    </figure>
   </div>
 </template>
 
@@ -24,6 +28,46 @@ export default {
     },
     dayCalendar() {
       return convertFromObjectToDate(this.day)
+    },
+    moonPic() {
+      const pic = {
+        1: {
+          0: '/assets/moon/1.png',
+          10: '/assets/moon/2.png',
+          20: '/assets/moon/3.png',
+          30: '/assets/moon/4.png',
+          40: '/assets/moon/5.png',
+          50: '/assets/moon/6.png',
+          60: '/assets/moon/7.png',
+          70: '/assets/moon/8.png',
+          80: '/assets/moon/9.png',
+          90: '/assets/moon/10.png',
+          100: '/assets/moon/11.png',
+        },
+        '-1': {
+          0: '/assets/moon/23.png',
+          10: '/assets/moon/22.png',
+          20: '/assets/moon/21.png',
+          30: '/assets/moon/20.png',
+          40: '/assets/moon/19.png',
+          50: '/assets/moon/18.png',
+          60: '/assets/moon/17.png',
+          70: '/assets/moon/16.png',
+          80: '/assets/moon/15.png',
+          90: '/assets/moon/14.png',
+          100: '/assets/moon/13.png',
+        },
+      }
+      let result = null
+      result = pic[this.day.moon_direction]
+      console.log(this.day.moon)
+      for (const one in result) {
+        if (this.day.moon * 100 > one) {
+          result = one
+        }
+      }
+      console.log('result = ', result)
+      return pic[this.day.moon_direction][result]
     },
   },
   created() {},
