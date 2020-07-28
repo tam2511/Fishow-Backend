604fa54ccd5fcdd6def06aad1213d50f00cd3cad
DamirAkzhigitov <damir.ckelet@gmail.com>
2020-07-28 20:06:43 +0300

    прогноз на главной странице

--- fishow_django/frontend3_1/frontend3/components/Sidebar/MiniPrediction.vue
+++ fishow_django/frontend3_1/frontend3/components/Sidebar/MiniPrediction.vue
@@ -1,11 +1,31 @@
 <template>
   <div class="miniPrediction">
-    Hello World!
+    {{ readyData }}
   </div>
 </template>
 
 <script>
-export default {}
+import { mapActions, mapState } from 'vuex'
+import { convertDataFromServer } from '@/assets/js/convertDataFromServer'
+export default {
+  computed: {
+    readyData() {
+      return convertDataFromServer(this.predictions)
+    },
+    ...mapState('prediction', ['predictions']),
+  },
+  created() {
+    const date = '2020-07-18'
+    const url = encodeURI(
+      `/predictionten/?areal=Московская область&date=${date}&city=Москва&fish=Щука`
+    )
+    this.getPrediction(url)
+    console.log('this.readyData = ', this.readyData)
+  },
+  methods: {
+    ...mapActions('prediction', { getPrediction: 'getPrediction' }),
+  },
+}
 </script>
 
 <style scoped></style>
