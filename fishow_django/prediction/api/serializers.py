9b8d647e5d24ef9ba0d1309eeff99210e03adcd3
AlecanderOfMacedon <tam2511@mail.ru>
2020-08-02 16:35:35 +0300

    ебанная русская "с"

--- fishow_django/prediction/api/serializers.py
+++ fishow_django/prediction/api/serializers.py
@@ -60,7 +60,7 @@ class PredictionSerializer(serializers.ModelSerializer):
             TextGenerator.update_stage(instance.city, instance.areal)
         return TextGenerator.get_day_prediction_desc(instance.date, instance.fish)
 
-    def get_prediсtion_brief(self, instance):
+    def get_prediction_brief(self, instance):
         if not TextGenerator.check_stage(instance.city, instance.areal):
             data = Prediction.objects.filter(city=instance.city, areal=instance.areal)
             TextGenerator.set_data(data)
