f1c67d22e443ec3e9cf0aca237ce0c4a6a56cf27
AlecanderOfMacedon <tam2511@mail.ru>
2020-09-20 13:38:42 +0300

    корректор

--- spider/correct.py
+++ spider/correct.py
@@ -1,6 +1,11 @@
 import datetime
+import numpy as np
 
-from spider.utils import read_config
+from spider.utils import read_config, average_time
+
+DIGIT_KEYS = {'temperature', 'wind', 'gust', 'pressure', 'humidity', 'uv_index'}
+MOON_KEYS = {'moon', 'moon_direction'}
+SUN_KEYS = {'sun_up', 'sun_down'}
 
 
 class DataError(Exception):
@@ -9,6 +14,34 @@ class DataError(Exception):
 
     def __str__(self):
         return 'DataError: {}'.format(self.message)
+
+
+def average_(left, right, key):
+    left_value = left[key]
+    right_value = right[key]
+    if not left_value or not right_value:
+        return None
+    if key in DIGIT_KEYS:
+        average = np.array(list(map(int, right_value.split(',')))) - np.array(list(map(int, left_value.split(','))))
+        average = average / 2
+        average = ','.join(map(str, average))
+        return average
+    elif key == 'phenomenon':
+        return left_value
+    elif key in MOON_KEYS:
+        left_moon = float(left['moon'])
+        right_moon = float(right['moon'])
+        left_moon_direction = int(left['moon_direction'])
+        right_moon_direction = int(right['moon_direction'])
+        moon_average = left_moon * left_moon_direction + right_moon * right_moon_direction
+        if key == 'moon':
+            return abs(moon_average)
+        return 1 if moon_average >= 0 else -1
+    elif key in SUN_KEYS:
+        return average_time(left_value, right_value)
+    else:
+        IndexError('Bad key for averaging data: {}'.format(key))
+    return None
 
 
 class Corrector:
@@ -27,13 +60,42 @@ class Corrector:
         date = data[idx]['date']
         city = data[idx]['city']
         extra_data = self.mysql.select(table_name='prediction_prediction',
-                                       where={'date': [date, date + datetime.timedelta(days=1)], 'city': city})
+                                       where={'date': [date, date - datetime.timedelta(days=1)], 'city': city})
 
         if len(extra_data) == 0:
             raise DataError('DataError: can\'t find data in table for date = {}, city = .'.format(date, city))
+        else:
+            current_data = [_ for _ in extra_data if _['date'] == date]
+            yesterday_data = [_ for _ in extra_data if not _['date'] == date]
+
+            if len(current_data) == 0 and idx < len(data) - 1:
+                tommorow_data = data[idx + 1]
+                average_data = average_(yesterday_data[0], tommorow_data, key)
+                if not average_data:
+                    raise DataError('Bad averaging data for data {} and key {}'.format(current_data, key))
+                data[idx][key] = average_data
+            elif len(yesterday_data) == 0:
+                data[idx][key] = current_data[0][key]
+            else:
+                raise DataError(
+                    'Last idx can\'t correct without nex idx for data {} and key {}'.format(current_data, key))
 
     def correct_(self, data, key):
         if not data[0][key]:
             if not self.mysql:
                 raise DataError('Don\'t find key {} for first day and mysql corrector.'.format(key))
             else:
+                self.correct_mysql_(data, key, 0)
+        if not data[-1][key]:
+            if not self.mysql:
+                raise DataError('Don\'t find key {} for first day and mysql corrector.'.format(key))
+            else:
+                self.correct_mysql_(data, key, len(data) - 1)
+        for idx in range(1, len(data) - 1):
+            if data[idx][key]:
+                continue
+            average_data = average_(data[idx - 1], data[idx + 1], key)
+            if not average_data:
+                self.correct_mysql_(data, key, idx)
+            else:
+                data[idx][key] = average_data
--- spider/utils.py
+++ spider/utils.py
@@ -4,3 +4,13 @@ import json
 def read_config(config_path):
     with open(config_path, 'r', encoding='UTF-8') as config_file:
         return json.load(config_file)
+
+
+def average_time(left_time, right_time):
+    time1 = left_time.split(':')
+    time2 = right_time.split(':')
+    time1 = int(time1[0]) * 60 + int(time1[1])
+    time2 = int(time2[0]) * 60 + int(time2[1])
+    mean_time = (time2 - time1) // 2 + time1
+    mean_time = ':'.join(map(str, [mean_time // 60, mean_time % 60]))
+    return mean_time
