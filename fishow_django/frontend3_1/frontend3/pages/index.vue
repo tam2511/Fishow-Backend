069806694f458643a1403818cb5e0fb3a70f277c
DamirAkzhigitov <damir.ckelet@gmail.com>
2020-09-26 11:35:13 +0300

    бесконечный скролл

--- fishow_django/frontend3_1/frontend3/pages/index.vue
+++ fishow_django/frontend3_1/frontend3/pages/index.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="tile is-vertical is-8">
     <h4 class="title is-4">Последние записи</h4>
-    <div class="tile is-parent is-vertical space-left0">
+    <div
+      v-infinite-scroll="loadMore"
+      infinite-scroll-disabled="busy"
+      infinite-scroll-distance="10"
+      class="tile is-parent is-vertical space-left0"
+    >
       <article
         v-for="blog in blogs"
         :key="blog.id"
@@ -24,12 +29,38 @@ export default {
   data() {
     return {
       blogs: null,
+      data: [],
+      count: 0,
+      busy: false,
+      next: null,
     }
   },
   mounted() {
     this.$axios.get('/blogs/').then((res) => {
       this.blogs = res.data.results
+      this.next = res.data.next
+      console.log('this.next = ', this.next)
     })
+  },
+  methods: {
+    loadMore() {
+      this.busy = true
+
+      setTimeout(() => {
+        if (this.next) {
+          const url = this.next.split('api/')[1]
+          this.$axios.get(`/${url}`).then((res) => {
+            this.blogs.push(...res.data.results)
+            if (res.data.next) {
+              this.next = res.data.next
+            } else {
+              this.next = null
+            }
+          })
+        }
+        this.busy = false
+      }, 1000)
+    },
   },
   head: {
     title: 'Прогноз клева рыбы, общение и новости | Fishow',
