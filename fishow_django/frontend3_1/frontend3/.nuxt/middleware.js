325406f12546677e91494d49f4e3c9b393fdecd4
DamirAkzhigitov <damir.ckelet@gmail.com>
2021-01-27 20:31:31 +0300

    вернул как было

--- fishow_django/frontend3_1/frontend3/.nuxt/middleware.js
+++
@@ -1,6 +0,0 @@
-const middleware = {}
-
-middleware['loggedIn'] = require('../middleware/loggedIn.js')
-middleware['loggedIn'] = middleware['loggedIn'].default || middleware['loggedIn']
-
-export default middleware
