af48063dc3cf772284fd170c4c99d19db0e986c7
vd <irnbru007@mail.ru>
2021-01-31 15:01:16 +0300

    serialize woterpleses + nature

--- fishow_django/space/api/serializers.py
+++ fishow_django/space/api/serializers.py
@@ -5,27 +5,27 @@ from django.utils.timesince import timesince
 from blogs.api.serializers import BlogSerializerSlug
 
 class Waterplace_natureSerializer(serializers.ModelSerializer):
-    slug = serializers.StringRelatedField(many=True)
-#     blogs = serializers.StringRelatedField(many=True)
-#     reports = serializers.StringRelatedField(many=True)
-#     news = serializers.StringRelatedField(many=True)
+    slug = serializers.SlugField(read_only=True)
+    blogs = serializers.StringRelatedField(many=True)
+    reports = serializers.StringRelatedField(many=True)
+    news = serializers.StringRelatedField(many=True)
     class Meta:
         model = Waterplace_nature
         fields = '__all__'
 
 class Waterplace_costSerializer(serializers.ModelSerializer):
     slug = serializers.SlugField(read_only=True)
-#     blogs = BlogSerializerSlug(many=True)
-#     reports = serializers.StringRelatedField(many=True)
-#     news = serializers.StringRelatedField(many=True)
+    blogs = serializers.StringRelatedField(many=True)
+    reports = serializers.StringRelatedField(many=True)
+    news = serializers.StringRelatedField(many=True)
     class Meta:
         model = Waterplace_cost
         fields = '__all__'
 
 class RegionSerializer(serializers.ModelSerializer):
     slug = serializers.SlugField(read_only=True)
-    #waterplace_cost = serializers.StringRelatedField(many=True)
-    #waterplace_nature = serializers.StringRelatedField(many=True)
+    waterplace_cost = serializers.StringRelatedField(many=True)
+    waterplace_nature = serializers.StringRelatedField(many=True)
     class Meta:
         model = Region
         fields = '__all__'
