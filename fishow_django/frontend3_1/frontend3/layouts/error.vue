5f5c67b8fe34c9351e68ac9451582779ea70eb08
DamirAkzhigitov <damir.ckelet@gmail.com>
2021-02-01 20:51:04 +0300

    обработка ошибки неизвестной страницы

--- fishow_django/frontend3_1/frontend3/layouts/error.vue
+++ fishow_django/frontend3_1/frontend3/layouts/error.vue
@@ -1,9 +1,5 @@
 <template>
-  <div class="container">
-    <h1 v-if="error.statusCode === 404">Page not found</h1>
-    <h1 v-else>An error occurred</h1>
-    <nuxt-link to="/">Home page</nuxt-link>
-  </div>
+  <div class="container"></div>
 </template>
 
 <script>
@@ -14,6 +10,16 @@ export default {
       type: Object,
       required: true,
     },
+  },
+  mounted() {
+    this.$buefy.toast.open({
+      duration: 2000,
+      message: 'Такой страницы пока что нету.',
+      type: 'is-danger',
+    })
+    setTimeout(() => {
+      this.$router.push({ name: 'index' })
+    }, 1000)
   },
 }
 </script>
