2d53352e1ee63959c7370620280df54709249404
data-sachez-2511 <tam2511@mail.ru>
2021-03-05 09:53:52 +0300

    for merge

--- assets/load2mysql.py
+++ assets/load2mysql.py
@@ -11,7 +11,6 @@ import string
 ALPHANUMERIC_CHARS = string.ascii_lowercase + string.digits
 STRING_LENGTH = 6
 
-
 def generate_random_string(chars=ALPHANUMERIC_CHARS, length=STRING_LENGTH):
     return "".join(random.choice(chars) for _ in range(length))
 
@@ -58,3 +57,4 @@ while True:
 
     if i >= len(data):
         break
+
