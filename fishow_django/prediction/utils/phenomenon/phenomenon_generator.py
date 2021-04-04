7a903c92161cb45ec6b7779f600f4f632a79cdcf
data-sachez-2511 <tam2511@mail.ru>
2021-04-04 09:39:04 +0300

    test

--- fishow_django/prediction/utils/phenomenon/phenomenon_generator.py
+++ fishow_django/prediction/utils/phenomenon/phenomenon_generator.py
@@ -15,9 +15,8 @@ class PhenomenonTextGenerator:
     @staticmethod
     def get_day_desc(data, date, fish):
         observe_dates = [date - datetime.timedelta(days=day) for day in range(1, 3)]
-        day_data = sum([(_.temperature, _.phenomenon) for _ in data if _.date == date and _.fish == fish], [])
-        filtred_data = sum([(_.temperature, _.phenomenon) for _ in data if _.date in observe_dates and _.fish == fish],
-                           [])
+        day_data = [(_.temperature, _.phenomenon) for _ in data if _.date == date and _.fish == fish]
+        filtred_data = [(_.temperature, _.phenomenon) for _ in data if _.date in observe_dates and _.fish == fish]
         print('day_data: {}'.format(day_data))
         print('filtred_data: {}'.format(filtred_data))
         return {}
