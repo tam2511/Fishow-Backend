202087ddec677173919005478c8f534618381330
vd <irnbru007@mail.ru>
2021-07-18 12:40:10 +0300

    вот сделал теги как надо фронту

--- fishow_django/blogs/api/serializers.py
+++ fishow_django/blogs/api/serializers.py
@@ -4,7 +4,8 @@ from other_models.models import Tags
 from datetime import datetime,timezone
 from django.utils.timesince import timesince
 from users.api.serializers import ShortUserDisplaySerializer
-
+from django.core.exceptions import ObjectDoesNotExist
+from other_models.api.views import TagsView
 
 class CommentSerializer(serializers.ModelSerializer):
     author = ShortUserDisplaySerializer(read_only=True)
@@ -53,9 +54,58 @@ class CommentSerializer(serializers.ModelSerializer):
     def get_comments_slug(self, instance):
         return instance.blog.slug
 
+# class CreatableSlugRelatedField(serializers.SlugRelatedField):
+#
+#     def to_internal_value(self, data):
+#         try:
+#             return self.get_queryset().get_or_create(**{self.slug_field: data})[0]
+#         except ObjectDoesNotExist:
+#             self.fail('does_not_exist', slug_name=self.slug_field, value=smart_text(data))
+#         except (TypeError, ValueError):
+#             self.fail('invalid')
+
+# class CreatableSlugRelatedField(serializers.SlugRelatedField):
+#     def to_internal_value(self, data):
+#         try:
+#             print(self)
+#             return self.get_queryset().get(**{self.slug_field: data})
+#         except ObjectDoesNotExist:
+#             Tags.objects.create(author=instance.user,**{self.slug_field: data})
+#             return ""#self.get_queryset().create(**{self.slug_field: data})  # to create the object
+#         except (TypeError, ValueError):
+#             self.fail('invalid')
+
+class CreatableSlugRelatedField(serializers.SlugRelatedField):
+    def to_internal_value(self, data):
+        try:
+            request = self.context.get("request")
+#             print(len(data))
+            return self.get_queryset().get(**{self.slug_field: data})
+        except ObjectDoesNotExist:
+            return Tags.objects.create(author=request.user,**{self.slug_field: data})
+            #return self.get_queryset().create(**{self.slug_field: data})  # to create the object
+        except (TypeError, ValueError):
+            self.fail('invalid')
+
+# class CreatableSlugRelatedField(serializers.SlugRelatedField):
+#
+#     def to_internal_value(self, data):
+#         print(data)
+#         try:
+#             return Tags.objects.get_or_create(data)[1]
+#             #return self.get_queryset().get_or_create(**{self.slug_field: data})[0]
+#         except:
+#             print('err')
+#             return ""
+#         except ObjectDoesNotExist:
+#             self.fail('does_not_exist', slug_name=self.slug_field, value=smart_text(data))
+#         except (TypeError, ValueError):
+#             self.fail('invalid')
+
 class BlogSerializer(serializers.ModelSerializer):
     author = ShortUserDisplaySerializer(read_only=True)
-    tags = serializers.SlugRelatedField(many=True,allow_null=True,slug_field='name',queryset=Tags.objects.all())
+    #tags = serializers.SlugRelatedField(many=True,allow_null=True,slug_field='name',queryset=Tags.objects.all())
+    tags = CreatableSlugRelatedField(many=True,allow_null=True,slug_field='name',queryset=Tags.objects.all())
     created_at = serializers.SerializerMethodField()
     likes_count = serializers.SerializerMethodField()
     dislikes_count = serializers.SerializerMethodField()
--- fishow_django/other_models/api/views.py
+++ fishow_django/other_models/api/views.py
@@ -25,30 +25,30 @@ class TagsView(viewsets.ModelViewSet):
         filter_backends = [filters.SearchFilter]
         search_fields = ['name']
 
-        def perform_create(self, serializer):
-                    if not self.request.user.is_anonymous:
-                        serializer.save(author=self.request.user)
-
-        def create(self, request, *args, **kwargs):
-                 data=request.data
-                 many = True if isinstance(data, list) else False
-
-                 if many==True:
-                    if len(data)>20:
-                        serializer_context = {'message':'error max tags'}
-                        return Response(serializer_context)
-                    data=[]
-                    for item in request.data:
-                        if self.get_serializer(data=item, many=False).is_valid()==True:
-                            data.append(item)
-                    if len(data)==0:
-                        serializer_context = {'message':'all tags exist'}
-                        return Response(serializer_context)
-                 serializer = self.get_serializer(data=data, many=many)
-                 serializer.is_valid(raise_exception=True)
-                 self.perform_create(serializer)
-                 headers = self.get_success_headers(serializer.data)
-                 return Response({'message':'ok'}, status=status.HTTP_201_CREATED, headers=headers)
+#         def perform_create(self, serializer):
+#                     if not self.request.user.is_anonymous:
+#                         serializer.save(author=self.request.user)
+#
+#         def create(self, request, *args, **kwargs):
+#                  data=request.data
+#                  many = True if isinstance(data, list) else False
+#
+#                  if many==True:
+#                     if len(data)>20:
+#                         serializer_context = {'message':'error max tags'}
+#                         return Response(serializer_context)
+#                     data=[]
+#                     for item in request.data:
+#                         if self.get_serializer(data=item, many=False).is_valid()==True:
+#                             data.append(item)
+#                     if len(data)==0:
+#                         serializer_context = {'message':'all tags exist'}
+#                         return Response(serializer_context)
+#                  serializer = self.get_serializer(data=data, many=many)
+#                  serializer.is_valid(raise_exception=True)
+#                  self.perform_create(serializer)
+#                  headers = self.get_success_headers(serializer.data)
+#                  return Response({'message':'ok'}, status=status.HTTP_201_CREATED, headers=headers)
 
 class Fishing_methodView(viewsets.ModelViewSet):
         queryset = Fishing_method.objects.all()
